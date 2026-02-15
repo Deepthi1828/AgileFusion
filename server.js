2eaa77a111428a05382de2a9bbfd12e8c357b531
Deepthi1828 <subravetideepthi@gmail.com>
2026-02-15 14:25:08 +0530

    server.js

--- server.js
+++ server.js
@@ -188,7 +188,7 @@ app.delete("/deleteTask/:id", async (req, res) => {
 });
 
 // ================= START SERVER =================
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(` Server running on http://localhost:${PORT}`);
